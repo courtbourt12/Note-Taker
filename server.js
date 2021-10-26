e1708d88b864f51d696c8161b4c601e37111bb42
courtney long <courtneylong877@gmail.com>
2021-10-26 07:29:29 -0400

    downloaded the nps files and got the loading page to connect to the html

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.port || 3003;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
