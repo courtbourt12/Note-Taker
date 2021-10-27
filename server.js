d3a1ef3008d39c446197baab0750302ee6237d2c
courtney long <courtneylong877@gmail.com>
2021-10-27 18:20:37 -0400

    Barely changed anything

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// app.get('/api/notes', (req,res) => {
-//     console.log(`${req.method} request received.`)
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
