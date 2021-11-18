e3a5743c9add2d77eabed313f446298c2a9e36e3
courtney long <courtneylong877@gmail.com>
2021-11-18 14:29:23 -0500

    Set up the functions to be used

---
+++ db/store.js
@@ -0,0 +1,17 @@
+// Function to get the notes from user input.
+
+getNotes = () => {
+
+}
+
+// Function to save the notes.  
+
+addNote = () => {
+
+}
+
+// Function to delete the notes.
+
+removeNote = () => {
+    
+}
